db5525b0f188d7b8e0b4a723b6fdb19b80a96c6a
Nikoruso <62753545+dostal83@users.noreply.github.com>
2021-06-11 04:17:36 +0300

    Update pr 6.1.py

--- pr 6.1.py
+++ pr 6.1.py
@@ -1,14 +1,18 @@
+#Классификатор на основе перцептрона
 import matplotlib.pyplot as plt
-import neurolab as nl
+import neurolab as nl # импорт библиотек
 
+# Значения ввода (контр. об) 
 input = [[0, 0], [0, 1], [1, 0], [1, 1]]
 target = [[0], [0], [0], [1]]
 
-net = nl.net.newp([[0,1], [0,1],], 1) 
+# Создаем нейронку 
+net = nl.net.newp([[0,1], [0,1],], 1) # 2 входа на 1 нейрон
 
-error_progress = net.train(input, target, epochs=100, show=10, lr=0.1)
+# Тренеруем сеть по принципу delta
+error_progress = net.train(input, target, epochs=100, show=10, lr=0.1) 
 
-
+# Визуалка
 plt.figure()
 plt.plot(error_progress)
 plt.xlabel('Number of epochs')
